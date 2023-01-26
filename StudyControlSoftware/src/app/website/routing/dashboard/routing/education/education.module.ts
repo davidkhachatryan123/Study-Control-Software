85796b299130f59f0cfc9bbb72f210d91c008074
davidkhachatryan359 <xdavit777@icloud.com>
2023-01-26 17:36:21 +0400

    Fixed polniy

--- StudyControlSoftware/src/app/website/routing/dashboard/routing/education/education.module.ts
+++ StudyControlSoftware/src/app/website/routing/dashboard/routing/education/education.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/website/shared/shared.module';
 
 import { EducationRoutingModule } from './education-routing.module';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   imports: [
     EducationRoutingModule,
     SharedModule
   ],
+  declarations: [ TestComponent ]
 })
 export class EducationModule { }
