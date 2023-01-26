8f364cfa2f1b77edf7bb6e316e5d701cd9ba3430
davidkhachatryan359 <xdavit777@icloud.com>
2023-01-26 14:14:33 +0400

    edited sidebar

--- StudyControlSoftware/src/app/website/consts/routes.ts
+++ StudyControlSoftware/src/app/website/consts/routes.ts
@@ -2,6 +2,8 @@ export enum routes {
   LOGIN = '/auth/login',
   REGISTER = '/auth/register',
   DASHBOARD = '/dashboard',
+  DASHBOARD_EDUCATION_FACULTY = '/dashboard/faculty',
+  DASHBOARD_EDUCATION_COURSE = '/dashboard/course',
   DASHBOARD_USERS_APP = '/dashboard/users/app',
   DASHBOARD_USERS_ADMINS = '/dashboard/users/admins'
 }
