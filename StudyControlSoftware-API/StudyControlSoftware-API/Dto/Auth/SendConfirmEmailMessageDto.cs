ac9b362962394ed78bc2b64b2a4bfb392efa0dec
davidkhachatryan359 <xdavit777@icloud.com>
2023-02-15 15:08:38 +0400

    Created API for Sending Confirmation Email and fixed bugs

---
+++ StudyControlSoftware-API/StudyControlSoftware-API/Dto/Auth/SendConfirmEmailMessageDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudyControlSoftware_API.Dto.Auth
+{
+    public class SendConfirmEmailMessageDto
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; init; } = null!;
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; init; } = null!;
+    }
+}
+
