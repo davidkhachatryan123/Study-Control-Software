b1d031c0fd83e8bb9e13dc1a7ff3509b04d02cc8
davidkhachatryan359 <xdavit777@icloud.com>
2023-02-15 13:26:52 +0400

    Fiexd Bugs

---
+++ StudyControlSoftware-API/StudyControlSoftware-API/Extensions/UrlHelperExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using StudyControlSoftware_API.Database.Models;
+using StudyControlSoftware_API.Dto.Auth;
+using StudyControlSoftware_API.Interfaces;
+
+namespace StudyControlSoftware_API.Extensions
+{
+    public static class UrlHelperExtension
+    {
+        public static async Task<string?> GenerateConfirmationEmailLinkAsync(this IUrlHelper url, IRepositoryManager repositoryManager, string username)
+        {
+            var email =
+                await repositoryManager.UserAuthentication.GetEmailAsync(username);
+            var token =
+                await repositoryManager.UserAuthentication.GenerateEmailConfirmToken(username);
+
+            return email == null
+                ? null
+                : url.Action("ConfirmEmail",
+                    "Account",
+                    new ConfirmEmailDto
+                    {
+                        Email = email,
+                        Token = token
+                    }, "https");
+        }
+    }
+}
+
